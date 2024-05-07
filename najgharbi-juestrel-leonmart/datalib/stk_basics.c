62c62105ebbc9c4962090547c026e13d81f120e0
Juan Jose Estrella Ramirez <128648875+juan-est145@users.noreply.github.com>
2024-05-07 21:59:38 +0200

    Fixed stk_basics issue

--- najgharbi-juestrel-leonmart/datalib/stk_basics.c
+++ najgharbi-juestrel-leonmart/datalib/stk_basics.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   stk_basics.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: antgalan <antgalan@student.42malaga.com    +#+  +:+       +#+        */
+/*   By: juestrel <juestrel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/01 18:40:27 by antgalan          #+#    #+#             */
-/*   Updated: 2023/01/23 12:08:46 by antgalan         ###   ########.fr       */
+/*   Updated: 2024/05/07 21:59:11 by juestrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,14 @@ void	stk_push(t_stack **stack, void *data)
 
 t_stack	*stk_pop(t_stack **stack)
 {
-	// TODO
+	t_stack *aux;
+
+	if (*stack == NULL)
+		return (NULL);
+	aux = *stack;
+	*stack = (*stack)->next;
+	aux->next = NULL;
+	return (aux);
 }
 
 void	stk_clear(t_stack **stack)
